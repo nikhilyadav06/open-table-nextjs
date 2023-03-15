be0e50cdbdd0a3f031ef1ff96dfbde1b9368a55b
Nikhil Yadav <nikhilyadav.nnn@gmail.com>
2023-03-15 21:43:15 +0530

    even more refactoring

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-import NavBar from '../../components/NavBar'
 import Description from './components/Description'
 import Header from './components/Header'
 import Images from './components/Images'
@@ -11,24 +9,18 @@ import Title from './components/Title'
 
 export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen text-gray-700">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <RestaurantNavBar />
-            <Title />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
-        </div>
-      </main>
-    </main>
+    <>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavBar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
+      </div>
+    </>
   )
 }
