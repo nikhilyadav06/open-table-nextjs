46287dcb820f1455584660387334873776491ece
Nikhil Yadav <nikhilyadav.nnn@gmail.com>
2023-03-18 22:22:52 +0530

    Added db and dynamic data

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,47 @@
 import Header from './components/Header'
 import RestaurantCard from './components/RestaurantCard'
+import { PrismaClient, Cuisine, Location, PRICE, Review } from '@prisma/client'
 
-export default function Home() {
+export interface RestaurantCardType {
+  id: number
+  name: string
+  main_image: string
+  cuisine: Cuisine
+  location: Location
+  price: PRICE
+  slug: string,
+  reviews: Review[]
+}
+
+const prisma = new PrismaClient()
+
+const fetchRestaurants =async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      slug: true,
+      location: true,
+      price: true,
+      reviews: true
+    }
+  })
+  return restaurants
+}
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants()
+  // console.log(restaurants)
+
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <RestaurantCard />
+        {restaurants.map(restaurant => (
+          <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+        ))}
       </div>
     </main>
   )
--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,28 +1,52 @@
+import { Cuisine, Location, PRICE, Review } from '@prisma/client'
 import Link from 'next/link'
+import { calculateReviewRatingAverage } from '../../../utils/calculateReviewRatingAverage'
+import Price from '../../components/Price'
+import Stars from '../../components/Stars'
 
-export default function RestaurantCard() {
+interface Restaurant {
+  id: number
+  location: Location
+  name: string
+  main_image: string
+  cuisine: Cuisine
+  price: PRICE
+  slug: string
+  reviews: Review[]
+}
+
+export default function RestaurantCard({ restaurant } : { restaurant: Restaurant }) {
+  const renderRatingText = () => {
+    const rating = calculateReviewRatingAverage(restaurant.reviews)
+
+    if (rating > 4) return 'Awesome'
+    else if (rating <= 4 && rating > 3) return 'Good'
+    else if (rating <= 3 && rating > 0) return 'Average'
+    else ''
+  }
+
   return (
-    <div className="border-b flex pb-5">
+    <div className="border-b flex pb-5 ml-4">
       <img
-        src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
+        src={restaurant.main_image}
         alt=""
-        className="w-44 rounded"
+        className="w-44 rounded object-cover"
       />
       <div className="pl-5">
-        <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
+        <h2 className="text-3xl">{restaurant.name}</h2>
         <div className="flex items-start">
-          <div className="flex mb-2">*****</div>
-          <p className="ml-2 text-sm">Awesome</p>
+          <Stars reviews={restaurant.reviews} />
+          <p className="ml-2 text-sm">{renderRatingText()}</p>
         </div>
         <div className="mb-9">
           <div className="font-light flex text-reg">
-            <p className="mr-4">$$$</p>
-            <p className="mr-4">Mexican</p>
-            <p className="mr-4">Ottawa</p>
+            <Price price={restaurant.price} />
+            <p className="mr-4 capitalize">{restaurant.cuisine.name}</p>
+            <p className="mr-4 capitalize">{restaurant.location.name}</p>
           </div>
         </div>
         <div className="text-red-600">
-          <Link href="/restaurant/m">View more information</Link>
+          <Link href={`/restaurant/${restaurant.slug}`}>View more information</Link>
         </div>
       </div>
     </div>
